0139efc5b69231f12e4d32484d167b5ac32cf1cb
Mike <michael@haleysoftware.com>
2013-08-13 05:24:25 -0700

    Worked on Apppurchasing and its layout

--- SpanishCards/src/main/java/com/haleysoft/spanish/AppPurchasing.java
+++ SpanishCards/src/main/java/com/haleysoft/spanish/AppPurchasing.java
@@ -3,7 +3,10 @@ package com.haleysoft.spanish;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.android.vending.billing.util.IabHelper;
 import com.android.vending.billing.util.IabResult;
@@ -13,7 +16,7 @@ import com.android.vending.billing.util.Purchase;
 /**
  * Created by mhaley on 8/6/13.
  */
-public class AppPurchasing extends Activity {
+public class AppPurchasing extends Activity implements View.OnClickListener {
 	private static final String MASTER_SETTINGS = "haley_master_set";
 	private SharedPreferences masterPref;
     public static final String LICENSE_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAhf7caMdtA37LLLSVLOrH4DbBU/8VpODxmetrLvVbimpZ7pzadIw/8UcZW9LFSihOcHQkel5Y3qu9QAFH/t87fwAYRbKeRY9nyZ8pNS4eqB+JpKKbI+jUm2aKZ4DYkTG8E8NP4w7FtFioH7+QTGBbLlZh0xz8mHNmHRPm50TIauyDq8x5ULh+me7XbJyfis2m3rzWAQIe9d2U51yMw51DEYN0+yccMKHTrhiA/72veinMfd6WBs3dGNT2jaVzUZ74Sr0iSPNSbuNftQBTwqI7ICFrDXsy5eUz6OsMfiQXKq+9HtgQAAlUuZZnVvGZUCCVY129Tlw6nNt0sODHOfOjmwIDAQAB";
@@ -26,7 +29,7 @@ public class AppPurchasing extends Activity {
 		super.onCreate(savedState);
 		this.setContentView(R.layout.shoplayout);
 		masterPref = getSharedPreferences(MASTER_SETTINGS, MODE_PRIVATE);
-
+		setWaitScreen(true);
 		inAppCheck();
 		checkBuys();
 	}
@@ -40,7 +43,7 @@ public class AppPurchasing extends Activity {
 
 	private void inAppCheck() {
 		billHelper = new IabHelper(this, AppPurchasing.LICENSE_KEY);
-		billHelper.enableDebugLogging(true);
+		billHelper.enableDebugLogging(true); //Need to change to false before release
 		billHelper.startSetup(new IabHelper.OnIabSetupFinishedListener() {
 			public void onIabSetupFinished(IabResult result) {
 				if (!result.isSuccess()) {
@@ -69,10 +72,13 @@ public class AppPurchasing extends Activity {
 							AppPurchasing.verifyDeveloperPayload(removeAdsPurchase));
 					masterPref.edit().putBoolean("remove_ads", adsRemoved).commit();
 
-			/*
-			//for Items that are consumed in replacement of setting the value
-			billHelper.consumeAsync(inventory.getPurchase(AppPurchasing.SKU_ADS), billConsumeFinishedListener);
-			*/
+					/*
+					//for Items that are consumed in replacement of setting the value
+					billHelper.consumeAsync(inventory.getPurchase(AppPurchasing.SKU_ADS), billConsumeFinishedListener);
+					*/
+
+					checkBuys();
+					setWaitScreen(false);
 				}
 			};
 
@@ -97,10 +103,22 @@ public class AppPurchasing extends Activity {
 		} else {
 			adButton.setClickable(true);
 			adButton.setText(R.string.shopadbuttonno);
+			adButton.setOnClickListener(this);
 		}
 
 		if (!masterPref.getBoolean("buy_okay", false)) {
 			adButton.setClickable(false);
+		}
+	}
+
+	private void setWaitScreen(boolean state) {
+		TextView wait = (TextView) this.findViewById(R.id.shop_Wait);
+		if (wait != null) {
+			wait.setVisibility(state ? View.VISIBLE : View.GONE);
+		}
+		LinearLayout adLayout = (LinearLayout) this.findViewById(R.id.shop_AdLayout);
+		if (adLayout != null) {
+			adLayout.setVisibility(state ? View.INVISIBLE : View.VISIBLE);
 		}
 	}
 
@@ -110,5 +128,13 @@ public class AppPurchasing extends Activity {
 		String payload = purchase.getDeveloperPayload();
 
 		return isReal;
+	}
+
+	public void onClick(View v) {
+		switch (v.getId()) {
+			case R.id.shop_adButton:
+				
+				break;
+		}
 	}
 }
